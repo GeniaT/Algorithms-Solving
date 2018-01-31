1d6d745aad164e97344b416568835ffe310ec81c
Genia <gtchistiakov@gmail.com>
2018-01-31 19:20:58 +1000

    Hackerrank cut the sticks algorithm

---
+++ HackerRank/Implementation/Cut-the-sticks.js
@@ -0,0 +1,26 @@
+function cutTheSticks(arr) {
+  let mini = arr[0];
+
+  arr.sort((a,b) => a - b);
+  console.log(arr.length)
+
+  function oneCut(arr) {
+    arr.forEach((x, index) => {
+      arr[index] -= mini;
+      if (arr[index] < 0) {
+        arr[index] = 0;
+      }
+    });
+
+    arr.splice(0,arr.lastIndexOf(0) + 1); //arr is modified.
+
+    console.log(arr.length);
+    if (arr.length > 1) {
+      oneCut(arr);
+    }
+  }
+    oneCut(arr);
+}
+
+// cutTheSticks([5, 4, 4, 2, 2, 8]); //6 (start nr of sticks) 4 2 1
+cutTheSticks([1, 2, 3, 4, 3, 3, 2, 1]); //8 6 4 1
